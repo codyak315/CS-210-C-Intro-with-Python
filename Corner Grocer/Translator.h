c2c3766f4f2e50a9895842ad1bf40c281da352b1
Szyx9 <99836247+Szyx9@users.noreply.github.com>
2022-02-16 15:07:17 -0500

    Add files via upload

---
+++ Corner Grocer/Translator.h
@@ -0,0 +1,50 @@
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+
+using namespace std;
+
+class Translator {
+
+public:
+
+// Constructor for Translator object.
+Translator();
+
+/*
+Description:
+	To call this function, simply pass the function name in Python that you wish to call.
+Example:
+	callProcedure("printsomething");
+Output:
+	Python will print on the screen: Hello from python!
+Return:
+	None
+*/
+void CallProcedure(string pName);
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("PrintMe","Test");
+Output:
+	Python will print on the screen:
+		You sent me: Test
+Return:
+	100 is returned to the C++
+*/
+int callIntFunc(string proc, string param);
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("doublevalue",5);
+Return:
+	25 is returned to the C++
+*/
+int callIntFunc(string proc, int param);
+
+};
+
+#endif
